64416a145608f533440a6c593aefb599b1773069
dantranucsd <33334144+dantranucsd@users.noreply.github.com>
2019-02-11 20:11:11 -0800

    used session.json for the task list

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@
 /*
  * GET home page.
  */
-
+var session = require('../session.json');
+ 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index',{
+	  "sessions": session.sessions,
+  });
 };
