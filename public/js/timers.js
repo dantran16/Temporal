5e9df22e806bfa673b811b07f7e27adfadc8f936
dantranucsd <33334144+dantranucsd@users.noreply.github.com>
2019-02-14 14:37:12 -0800

    got the timer to work

--- public/js/timers.js
+++ public/js/timers.js
@@ -1,40 +1,53 @@
 //global variables
-var timer = false;
-var time;
-var counter = 100;
-var timestring = "";
-
-
+var counter = 0;
+var timeleft;
+$(document).ready(function() {
+	start();
+})
 function start(){
-	if(!timer){
-		timer = true;
-		countdown();
+	//timeleft = document.getElementById("time");
+	var timestring = $("#time").text();
+	console.log(typeof timestring);
+	var hr = parseInt(timestring.substring(0,2));
+	console.log(hr + " hrs");
+	var minutes = parseInt(timestring.substring(4,6));
+	console.log(minutes + " minutes");
+	var seconds = parseInt(timestring.substring(7,9));
+	console.log(seconds + " seconds");
+	timeleft = hr * 3600 + minutes * 60 + seconds;
+	console.log(timeleft);
+	$("#time").html(convertSeconds(timeleft));
+	
+	function timer(){
+      if(timeleft==0){
+		timeleft = 0
+	  }
+	  else{
+		timeleft--;
+	  }
+	  $("#time").html(convertSeconds(timeleft));
 	}
+	
+	setInterval(timer, 1000);
 }
 
-function countdown(){
-	document.getElementById("time").value = stringtimer(counter);
-	timestring = stringTimer(counter);
-	counter--;
-	time = setTimeout("countdown();", 1000);
-	if(time==0){
-		stop();
+function convertSeconds(seconds){
+	var hrs = Math.floor(seconds/3600);
+	if(hrs < 10){
+		var seconddigit = hrs;
+		hrs = '0' + seconddigit;
 	}
-}
-
-function stop(){
-	timer = false;
-	clearTimeOut(time);
-}
-
-function clear(){
-	timer = false;
-	time = 0;
-}
-
-function stringtimer(nseconds){
-	var hours = Math.floor(nseconds/3600);
-	var minutes = Math.floor(nseconds / 60);
-	var seconds = nseconds % 60;
-	return hours + ":" minutes + ":" + seconds;
+	var remainingseconds = seconds % 3600;
+	var min = Math.floor(remainingseconds/60);
+	if(min < 10){
+		var seconddigit = min;
+		min = '0' + seconddigit;
+	}
+	var sec = remainingseconds % 60;
+	if(sec < 10){
+		var seconddigit = sec;
+		sec = '0'+ seconddigit;
+	}
+	var string = hrs + ':' + min + ':' + sec;
+	return string;
 }
