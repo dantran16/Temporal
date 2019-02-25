218b66afdfa29e213c9d6b2a08c0ae578a92c825
Angelica Ramos <amr052@ucsd.edu>
2019-02-25 14:43:00 -0800

    clock fixed

--- public/js/newSession.js
+++ public/js/newSession.js
@@ -116,6 +116,27 @@ function addTask() {
   location.replace(url + "/index");
   location.replace(url);
 }
+function timePicker{
+	var selection = "";
+	var i = 0;
+	for(var i = 0; i < 23; i++)
+	{
+	    var j = zeroFill(i, 2);
+	    selection += "<option value='"+ j +"00'>"+ j + ":00" + "</option>";
+	        selection += "<option value='"+ j +"30'>"+ j + ":30" + "</option>";
+	}
+	$("select").html(selection);
+	function zeroFill( number, width )
+	{
+	  width -= number.toString().length;
+	  if ( width > 0 )
+	  {
+	    return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+	  }
+	  return number + ""; // always return a string
+	}
+
+}
 
 
 /*
